d5476074e154561eeff1c0ef442bf3332f2c7ca6
Esmin Pasic <esminpasic@archstl.org>
2024-08-27 20:07:20 -0500

    New Components/ UI
    Added new Grid component to pages, and floating-nav, and its components

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,20 @@ import { Herr_Von_Muellerhoff } from "next/font/google";
 import Image from "next/image";
 import Hero from '../components/hero';
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import Grid from '../components/Grid';
+
 
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav />
+        <FloatingNav navItems={[
+          {name: 'Home', link: '/', icon: <FaHome />}
+        ]}/>
         <Hero></Hero>
+        <Grid></Grid>
       </div>
     </main>
   );
